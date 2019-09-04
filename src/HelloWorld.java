d252c375153a2916e2f9e849223b1670bac5ddb6
Ashur213 <ashur7@live.se>
2019-09-04 10:36:13 +0200

    Commit 9

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Ashur");
 			Scanner input = new Scanner(System.in);
 				
-			input.nextLine();
+
 			
-			System.out.println("vad är ditt användanamn");
+			System.out.println("vad är ditt användanamn?");
 			String name = input.nextLine();
 			System.out.println("ditt användanamn är: " + name);
-			
+			System.out.println("Hur gammal är du?");
+			String age = input.nextLine();
+			System.out.println("Din ålder är: " + age);
 	}
 
 }
