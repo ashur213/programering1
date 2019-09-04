f4b24d5ddf5ce57fcfc943a1083d3bf4d2044fee
Ashur213 <ashur7@live.se>
2019-09-04 10:43:17 +0200

    commit 14

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -12,6 +12,18 @@ public class HelloWorld {
 			System.out.println("Hur gammal är du?");
 			String age = input.nextLine();
 			System.out.println("Din ålder är: " + age);
+			System.out.println("Vad är din adress?");
+			String shorba = input.nextLine();
+			System.out.println("din adress är: " + shorba);
+			System.out.println("vad är din postnummer?");
+			String postnmr = input.nextLine();
+			System.out.println("din postnummer är: " + postnmr);
+			System.out.println("vilken stad bor du i?");
+			String stad = input.nextLine();
+			System.out.println("du bor i staden: " + stad);
+			System.out.println("vad är ditt telefonnummer?");
+			String telefon = input.nextLine();
+			System.out.println("Ditt telefonnummer är: " + telefon);
 	}
 
 }
